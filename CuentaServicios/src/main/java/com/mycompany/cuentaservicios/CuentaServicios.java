64920b76350aace656de38fffc4281c8b6441203
Juanes Jaramillo <44793875+ElBicu@users.noreply.github.com>
2026-02-27 14:36:33 -0500

    Resuelto

--- CuentaServicios/src/main/java/com/mycompany/cuentaservicios/CuentaServicios.java
+++ CuentaServicios/src/main/java/com/mycompany/cuentaservicios/CuentaServicios.java
@@ -16,6 +16,33 @@ public class CuentaServicios {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         
+        System.out.print("Ingresa la cantidad de agua del mes pasado: ");
+        int agualast=sc.nextInt();
+        
+        System.out.print("Ingresa la cantidad de luz del mes pasado: ");
+        int luzlast=sc.nextInt();
+        
+        System.out.print("Ingresa la cantidad de agua del mes actual: ");
+        int aguanow=sc.nextInt();
+        
+        System.out.print("Ingresa la cantidad de luz del mes actual: ");
+        int luznow=sc.nextInt();
+        
+        int agualastT=agualast*1500;
+        int aguanowT=aguanow*1500;
+        
+        int luzlastT=luzlast*130;
+        int luznowT=luznow*130;
+        
+        int aguaT=aguanowT-agualastT;
+        int luzT=luznowT-luzlastT;
+        
+        int serviciosT=aguaT+luzT;
+        
+        System.out.println("Total de servicios a pagar:$ "+serviciosT);
+        
+        
+        
         
         sc.close();
     }
